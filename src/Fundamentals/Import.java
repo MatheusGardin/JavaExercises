0a089e5c1638cf663bce9cb524f6a437d5e04a87
MatheusGardin <jackmatheus123@gmail.com>
2021-10-09 15:06:46 -0300

    Imports

---
+++ src/Fundamentals/Import.java
@@ -0,0 +1,20 @@
+package Fundamentals;
+
+import java.util.Date;
+// import javax.swing.JButton;
+
+public class Import {
+
+    public static void main(String[] args) {
+        java.lang.String b = "Good Evening";
+        java.lang.System.out.println(b);
+
+        String s = "Good Morning";
+        System.out.println(s);
+
+        Date date = new Date();
+        System.out.println(date);
+
+        // JButton button = new JButton();
+    }
+}
