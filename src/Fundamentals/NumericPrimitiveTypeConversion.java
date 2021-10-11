07e20d170a50668210345582182024bbc0bbea90
MatheusGardin <jackmatheus123@gmail.com>
2021-10-11 18:35:55 -0300

    Numeric Primitive Type Conversion

---
+++ src/Fundamentals/NumericPrimitiveTypeConversion.java
@@ -0,0 +1,23 @@
+package Fundamentals;
+
+import org.graalvm.compiler.asm.sparc.SPARCAssembler.FMOVcc;
+
+public class NumericPrimitiveTypeConversion {
+
+    public static void main(String[] args) {
+
+        double a = 1; // Implied
+        System.out.println(a);
+
+        float b = (float) 1.12345; // Explicit (CAST)
+        System.out.println(b);
+
+        int c = 4;
+        byte d = (byte) c; // Explicit (CAST)
+        System.out.println(d);
+
+        double e = 1.99999;
+        int f = (int) e; // Explicit (CAST)
+        System.out.println(f);
+    }
+}
