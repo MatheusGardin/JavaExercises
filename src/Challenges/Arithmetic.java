065bdde9baa8ceb55cf2d3e47b37a7dc3d28591a
MatheusGardin <jackmatheus123@gmail.com>
2021-10-12 15:25:41 -0300

    another way to resolve

--- src/Challenges/Arithmetic.java
+++ src/Challenges/Arithmetic.java
@@ -24,5 +24,23 @@ public class Arithmetic {
         double k = i / j;
 
         System.out.println(k);
+
+        // Another Way
+
+        double numA = Math.pow(6 * (3 + 2), 2);
+        double denA = 3 * 2;
+
+        double numB = (1 - 5) * (2 - 7);
+        double denB = 2;
+
+        double superiorA = numA / denA;
+        double superiorB = Math.pow(numB / denB, 2);
+
+        double superior = Math.pow(superiorA - superiorB, 3);
+        double inferior = Math.pow(10, 3);
+
+        double result = superior / inferior;
+
+        System.out.println("The result is " + result);
     }
 }
