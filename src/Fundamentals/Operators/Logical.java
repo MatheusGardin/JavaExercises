434cdbaa214bb350630a7436053200fc93ca8fb6
MatheusGardin <jackmatheus123@gmail.com>
2021-10-12 15:55:53 -0300

    logical operators

--- src/Fundamentals/Operators/Logical.java
+++ src/Fundamentals/Operators/Logical.java
@@ -4,6 +4,36 @@ public class Logical {
 
     public static void main(String[] args) {
 
-        
+        boolean condition1 = true;
+        boolean condition2 = false;
+
+        System.out.println(condition1 && condition2); // False
+        System.out.println(condition1 || condition2); // True
+        System.out.println(condition1 ^ condition2); // True
+        System.out.println(!condition1); // False
+        System.out.println(!condition2); // True
+
+        System.out.println("\nTruth Table E");
+        System.out.println(true && true); // True
+        System.out.println(true && false); // False
+        System.out.println(false && true); // False
+        System.out.println(false && false); // False
+
+        System.out.println("\nTruth Table OR");
+        System.out.println(true || true); // True
+        System.out.println(true || false); // True
+        System.out.println(false || true); // True
+        System.out.println(false || false); // False
+
+        System.out.println("\nTruth Table XOR");
+        System.out.println(true ^ true); // False
+        System.out.println(true ^ false); // True
+        System.out.println(false ^ true); // True
+        System.out.println(false ^ false); // False
+
+        System.out.println("\nTruth Table NOT");
+        System.out.println(!true); // False
+        System.out.println(!false); // True
+        System.out.println(!!false); // False
     }
 }
